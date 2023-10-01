057b84637fd17fae6e4f2440bd1aa83f7012fb0b
JoshWoods1 <woo19047@byui.edu>
2023-09-30 20:14:39 -0600

    DOM manipulation done

--- lesson2/scripts/bom.js
+++ lesson2/scripts/bom.js
@@ -6,7 +6,22 @@ const feedback = document.querySelector("#feedback");
 button.addEventListener("click", () => {
   if (input.value) {
     feedback.textContent = '';
-    console.log("clicked");
+    // console.log("clicked");
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+    li.innerHTML = input.value;
+    deleteButton.textContent = '❌';
+    li.append(deleteButton);
+    list.append(li);
+
+    deleteButton.addEventListener('click', function () {
+      list.removeChild(li);
+      input.focus();
+
+    input.focus();
+    input.value = '';
+    })
+
   } else {
     input.focus();
 
