beec0791b89ede6b498923cd9e2d660c8230049f
JoshWoods1 <woo19047@byui.edu>
2023-11-07 09:50:21 -0700

    test changes

---
+++ lesson5/scripts/prophets.js
@@ -0,0 +1,25 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+async function  getProphetData(){
+const response = await fetch(url);
+const data = await response.json();
+displayProphets(data.prophets);
+
+}
+
+getProphetData();
+
+function displayProphets(prophets) 
+{
+    prophets.forEach(prophet => {
+        const card = document.createElement('section');
+
+        const fullName = document.createElement('h2');
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`
+
+        card.appendChild(fullName);
+        cards.appendChild(card);
+
+    })
+}
