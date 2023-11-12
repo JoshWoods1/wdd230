43a323a2a1b645db26f70bfd9c2e0bef0ea68493
JoshWoods1 <woo19047@byui.edu>
2023-11-11 21:02:57 -0700

    learning activitites

---
+++ lesson5/scripts/weather.js
@@ -0,0 +1,32 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?lat=49.75115294578881&lon=6.638457404011544&units=imperial&appid=84fbae873dbd4636931d58506b2216a2";
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      let data = await response.json();
+      console.log(data);
+      displayResults(data)
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log("Error");
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconCode = data.weather[0].icon
+    const iconsrc = `https://openweathermap.org/img/w/${iconCode}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
